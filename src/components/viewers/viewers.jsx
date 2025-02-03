3aec37b706605fcff77cb84938b3ba42d1f927b4
LeandroAcipreste <logacipreste@gmail.comLeasou@22676clear>
2025-02-03 15:16:54 -0300

    add viwers

--- src/components/viewers/viewers.jsx
+++ src/components/viewers/viewers.jsx
@@ -3,8 +3,22 @@ import '../viewers/viewers.css'
 
 function Viewers() {
     return (
-        <div>
-            
+        <div className='container'>
+            <div className='container-box'>
+                <img src="../images/viewers-disney.png" alt="" className='container-box-img'/>
+            </div>
+            <div className='container-box'>
+                <img src="../images/viewers-pixar.png" alt="" className='container-box-img'/>
+            </div>
+            <div className='container-box'>
+                <img src="../images/viewers-marvel.png" alt="" className='container-box-img'/>
+            </div>
+            <div className='container-box'>
+                <img src="../images/viewers-starwars.png" alt="" className='container-box-img'/>
+            </div>
+            <div className='container-box'>
+                <img src="../images/viewers-national.png" alt="" className='container-box-img'/>
+            </div>
         </div>
     )
 }
